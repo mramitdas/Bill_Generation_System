8c72c40b2bcfed8a4f35566eff65a7e740255288
amit <mramitdas99@gmail.com>
2022-09-12 13:45:29 +0530

    added: database connectivity

---
+++ database.py
@@ -0,0 +1,16 @@
+import os
+import pymongo
+
+
+class Database:
+    def __init__(self, local=None):
+        if local:
+            from dotenv import load_dotenv
+            load_dotenv('.env')
+
+        client = pymongo.MongoClient(os.environ['Database_url'])
+        db = client[os.environ['Db_name']]
+        self.collection = db[os.environ['Table_name']]
+
+    def insertion(self, data):
+        self.collection.insert_one(data)
