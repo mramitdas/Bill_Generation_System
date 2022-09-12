c87d8a7cc6069e6a1e49d6cae5b543ea6429a8c0
amit <mramitdas99@gmail.com>
2022-09-12 14:00:06 +0530

    added: registration cloud entry

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template, request
+from database import Database
+import hashlib
 
 app = Flask(__name__)
+cloud = Database(local=True)
 
 
 @app.route('/login', methods=["POST", "GET"])
@@ -21,6 +24,11 @@ def register_user():
         name = data['name']
         email = data['email']
         password = data['pass']
+
+        cloud.insertion({"name": name,
+                         "_id": email,
+                         "secret": hashlib.sha256(password.encode('utf-8')).hexdigest(),
+                         "auth": "admin"})
 
         return render_template('register.html')
 
