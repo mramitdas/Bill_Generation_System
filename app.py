eaeb1b21062857781efe716b59539e23ad87aa37
amit <mramitdas99@gmail.com>
2022-09-12 13:27:18 +0530

    added: fetch register form data function

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -11,6 +11,18 @@ def login():
 @app.route('/register', methods=["POST", "GET"])
 def register():
     return render_template('register.html')
+
+
+@app.route('/register_user', methods=["POST", "GET"])
+def register_user():
+    if request.method == "POST":
+        data = request.form
+
+        name = data['name']
+        email = data['email']
+        password = data['pass']
+
+        return render_template('register.html')
 
 
 if __name__ == "__main__":
